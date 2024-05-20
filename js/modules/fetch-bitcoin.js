9a2839e503ac204926dca9ab8fdf1d503a60f517
Maristela Bispo <shestronger@gmail.com>
2024-05-19 23:33:34 -0300

    atualizado package

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -5,7 +5,6 @@ export default function initFetchBitcoin(){
   .then(json=>{
     const btcPreco = document.querySelector('.btc-preco');
     btcPreco.innerText = (1000/json.BRL.sell).toFixed(4);
-    console.log(json.BRL.sell)
   }).catch(erro=>{
     console.log(Error(erro))
   })
