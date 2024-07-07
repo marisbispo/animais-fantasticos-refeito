12ae662aa5aa7c3fbf1ab64897dc27f0ab8b6283
Maristela Bispo <shestronger@gmail.com>
2024-07-07 19:51:43 -0300

    funcionamento refatorado

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from './modules/modal.js';
 import ToolTip from './modules/tooltips.js';
 import DropdownMenu from './modules/dropdown-menu.js';
 import MenuMobile from './modules/menu-mobile.js';
-import initFuncionamento from './modules/funcionamento.js';
+import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import ScrollAnima from './modules/animacaoAoScroll.js';
@@ -36,8 +36,10 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
+const funcionamento = new Funcionamento('[data-semana]', 'aberto');
+funcionamento.init();
 
-initFuncionamento();
+
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 
